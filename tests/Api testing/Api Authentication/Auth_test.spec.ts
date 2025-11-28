099a84184964ce8b3477c67130a79afc2e29f91f
RoshanKumar <pldrroshan153@gmail.com>
2025-11-28 11:31:29 +0530

    Add Authentication testing file

---
+++ tests/Api testing/Api Authentication/Auth_test.spec.ts
@@ -0,0 +1,49 @@
+/*
+1) No Auth
+2)Basic auth (its have usename & password)
+3)Bearer Token (its have some token)
+4)Api key Auth (its have key & value)
+*/
+
+import {test,expect} from "@playwright/test"
+
+//Basic Auth
+
+test("Basic Auth using Get method with user& pass",async({request})=>{
+
+    const basicrequest = await request.get('https://httpbin.org/basic-auth/user/pass',{headers:
+                                                        {Authorization:`Basic `+Buffer.from("user:pass").toString('base64')}})
+
+    const basicresponse = await basicrequest.json()
+    console.log(basicresponse);
+    expect(basicrequest.ok()).toBeTruthy()
+    expect(basicrequest.status()).toBe(200)
+    expect(basicresponse).toHaveProperty('user')                                                    
+})
+
+//Bearer Token
+
+test("Bearer Token using get Method",async({request})=>{
+
+    const Bearertoken = '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'
+    const bearerequest = await request.get('https://github.com/user',{headers:{Authorization:`Bearer ${Bearertoken}`}})
+
+    expect(bearerequest.ok()).toBeTruthy()
+    expect(bearerequest.status()).toBe(200)  
+})
+
+
+//Api Key Auth
+
+test.only("Api key Auth using get method",async({request})=>{
+
+    const apikey = '029e9a59ffe25681436a1576e456b9c4'
+    const keyrequest = await request.get('https://openweathermap.org/price',{params:{appid:`${apikey}`}})
+
+    // const keyresponse = await keyrequest.json()
+    expect(keyrequest.ok()).toBeTruthy()
+    expect(keyrequest.status()).toBe(200)
+
+    // console.log(keyresponse);
+    
+})
